18ba53cd89d1bb3b0eaa297b5a5318d0cf4b61b7
tenyuikeda <124487739+zenigata0624@users.noreply.github.com>
2023-06-16 14:38:59 +0000

    GSAP導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,44 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "../stylesheets/application"
+
+import "popper.js";
+import "jquery";
+import { gsap } from "gsap";
+
+
 import "../stylesheets/index"
+import "../stylesheets/application"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener('turbolinks:load', function() {
+  const element = document.querySelector('.element');
+  element.style.opacity = '0';
+  element.style.transform = 'translateY(100px)';
+  let isVisible = false;
+　
+　 function handleScroll() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    
+   if (!isVisible && scrollTop > 100) {
+      gsap.to(element, {
+        opacity: 1,
+        y: 0,
+        duration: 1
+      });
+      isVisible = true;
+    } else if (isVisible && scrollTop <= 100) {
+      gsap.to(element, {
+        opacity: 0,
+        y: 100,
+        duration: 1
+      });
+      isVisible = false;
+    }
+  }
+
+  window.addEventListener('scroll', handleScroll);
+});
